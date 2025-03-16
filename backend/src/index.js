fc7401c22d400c1849da9e96db4fca400a8364e8
shayandutta <shayandutta23@gmail.com>
2025-03-16 22:57:17 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+//this is our api for frontend
+
+
+import express from "express";
+import "dotenv/config";
+
+import authRoutes from "./routes/authRoutes.js"
+import { connectDB } from "./lib/db.js";
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use("/api/auth", authRoutes);
+
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+    connectDB();
+})
---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post("/register", async (req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+router.post("/login", async (req,res)=>{
+    res.send("login");
+})
+
+export default router;
